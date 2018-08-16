3c7c7410d0f7303b54bac788d5929429718f71a0
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-08-16 12:47:25 -0400

    Fundamental practice

---
+++ Eclipse/Tutorial/src/Tutorial1.java
@@ -0,0 +1,20 @@
+
+//public class Tutorial1 
+//{
+//
+//	public static void main(String[] args)
+//	{
+//		System.out.println("Hello World!");
+//	}
+//
+//}
+
+public class Tutorial1{
+
+	   public static void main(String args[]){
+	      int a=20, b=30;
+
+	      if(b>a)
+	         System.out.println("b is greater");
+	      }
+	}
