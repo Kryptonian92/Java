90ab31957d4aeb4e702deec36cc138f983f1b0d6
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-09-05 12:12:40 -0400

    Completed the Basic13

---
+++ DojoJava/Fundamentals/src/ArraysTest.java
@@ -0,0 +1,8 @@
+//Test file for Arrays
+public class ArraysTest {
+	public static void main(String[] args){
+		Arrays methods = new Arrays();
+		methods.arrayNum();
+	}
+	
+}
---
+++ DojoJava/Fundamentals/src/LoopsTest.java
@@ -0,0 +1,7 @@
+//Test file for Loops
+public class LoopsTest {
+	public static void main(String[] args){
+		Loops methods = new Loops();
+		methods.forLoop();
+	}
+}
